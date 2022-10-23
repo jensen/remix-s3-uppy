8d9036329d7cefb1a6184474e8eceba7cc4b2626
Karl Jensen <karljensen@gmail.com>
2022-10-22 20:58:42 -0700

    Add progress/status bar

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,15 +2,18 @@ import { useEffect, useRef } from "react";
 import Uppy from "@uppy/core";
 import DragDrop from "@uppy/drag-drop";
 import AwsS3 from "@uppy/aws-s3";
+import StatusBar from "@uppy/status-bar";
 
 import type { LinksFunction } from "@remix-run/node";
 
 import uppyCoreStyles from "@uppy/core/dist/style.css";
 import uppyDragDropStyles from "@uppy/drag-drop/dist/style.css";
+import uppyStatusBarStyles from "@uppy/status-bar/dist/style.css";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: uppyCoreStyles },
   { rel: "stylesheet", href: uppyDragDropStyles },
+  { rel: "stylesheet", href: uppyStatusBarStyles },
 ];
 
 export default function Index() {
@@ -18,11 +21,14 @@ export default function Index() {
 
   useEffect(() => {
     if (uppy.current === null) {
-      uppy.current = new Uppy({ autoProceed: true })
+      uppy.current = new Uppy()
         .use(DragDrop, {
           target: "#dropzone",
         })
-
+        .use(StatusBar, {
+          target: "#progress",
+          hideAfterFinish: false,
+        })
         .use(AwsS3, {
           companionUrl: "/api/companion",
         });
@@ -39,6 +45,7 @@ export default function Index() {
     >
       <div style={{ width: "65ch" }}>
         <div id="dropzone" style={{ marginBottom: "1rem" }}></div>
+        <div id="progress"></div>
       </div>
     </div>
   );
